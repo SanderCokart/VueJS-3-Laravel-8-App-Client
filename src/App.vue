42381907d2cab8d6b5e9ddfe054b0ae4f02fd1b4
Sander Cokart <cokart32@gmail.com>
2021-07-16 01:29:48 +0200

    progress commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,85 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
+    |
+    <router-link to="/login">Login</router-link>
   </div>
   <router-view/>
 </template>
 
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  setup () {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const theme = ref(localStorage.getItem('theme') ?? (prefersDarkMode ? 'dark' : 'light'));
+    document.documentElement.setAttribute('data-theme', theme.value);
+    localStorage.setItem('theme', theme.value);
+
+    return { theme };
+  }
+};
+</script>
+
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --bg-dark: #333;
+  --bg-dark-2: #222;
+  --bg-light: #EEE;
+  --bg-light-2: #DDD;
+  --text-dark: #fff;
+  --text-light: #000;
+  --accent: #ff5722;
+
+  --bg: var(--bg-light);
+  --bg-2: var(--bg-light-2);
+  --text: var(--text-light);
+}
+
+[data-theme="dark"] {
+  --bg: var(--bg-dark);
+  --bg-2: var(--bg-dark-2);
+  --text: var(--text-dark);
+}
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+
+div {
+  display: flex;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: var(--bg);
+  color: var(--text);
+  flex-direction: column;
 }
 
 #nav {
   padding: 30px;
+  gap: 10px;
+  justify-content: center;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: var(--text);
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: var(--accent);
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
@@ -13,13 +13,18 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
